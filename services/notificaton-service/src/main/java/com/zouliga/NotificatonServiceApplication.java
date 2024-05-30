c2bd39b5aa4be272046d45a152d5c7db418332c5
Abdellatif Zouliga <zuuliga@gmail.com>
2024-05-30 18:08:38 -0400

    created notification service

---
+++ services/notificaton-service/src/main/java/com/zouliga/NotificatonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zouliga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificatonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificatonServiceApplication.class, args);
+	}
+
+}
